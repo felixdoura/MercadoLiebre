c91be488f7976a8bb19a694020bdc69b85fdf0c6
Felix <felixdoura@hotmail.com>
2022-12-14 19:29:29 -0300

    cambie el puerto de entrada

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000
+// const port = 3000
 
 app.use (express.static("public"))
 app.get ('/', (req, res) => {
@@ -19,5 +19,8 @@ app.get ('/login', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html")) 
 });
 
-app.listen (port, () => 
-console.log("Esta corriendo el puerto "+port));
+const port = process.env.PORT || 3001;
+app.listen(port,()=>console.log("Servidor corriendo en el puerto ${port}"));
+
+// app.listen (port, () => 
+// console.log("Esta corriendo el puerto "+port));
